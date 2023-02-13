19eb3a324c4fce9049f04bd2b2d5f9f0e96ddf5f
Emmanuel Chucks <echucks19@gmail.com>
2023-02-13 03:14:19 +0000

    fix prisma name conflict

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client"
 
-const prisma = globalThis.prisma || new PrismaClient()
-if (import.meta.env.DEV) globalThis.prisma = prisma
+const prisma = globalThis.prismaClient || new PrismaClient()
+if (import.meta.env.DEV) globalThis.prismaClient = prisma
 
 export default prisma
