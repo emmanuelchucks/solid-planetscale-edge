7f30625b4c7e51df1516b94808b0573bc89966a0
Emmanuel Chucks <echucks19@gmail.com>
2023-02-14 03:46:28 +0000

    add cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
+import cloudflarePages from "solid-start-cloudflare-pages"
 import solid from "solid-start/vite"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solid({
+      adapter: cloudflarePages({}),
+    }),
+  ],
 })
