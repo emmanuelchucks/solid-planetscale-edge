7dbdcc1016bdb80f43045dac9e5970891f5f35b1
Emmanuel Chucks <echucks19@gmail.com>
2023-02-15 23:08:08 +0000

    change page title

--- src/root.tsx
+++ src/root.tsx
@@ -17,7 +17,7 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - Prisma & Planetscale</Title>
+        <Title>SolidStart - PlanetScale at the Edge</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
